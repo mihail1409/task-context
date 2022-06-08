53123d351b9d42250dfde90a35174e3d9ed3639a
mihail1409 <aleksandrov20012@mail.ru>
2022-06-08 16:58:04 +0300

    Задание по контекстам React

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,34 @@
 import { useState } from 'react';
 import { data, IItem } from './data';
 import './styles.css';
-
+import { ThemeProvider, useTheme } from './Context';
 type Theme = 'light' | 'dark';
-
 export function App() {
     const [currentTheme, setCurrentTheme] = useState<Theme>('light');
-
     function changeTheme() {
         setCurrentTheme(currentTheme === 'light' ? 'dark' : 'light');
     }
-
     const className = `app app_${currentTheme}`;
     return (
-        <div className={className}>
-            <button onClick={changeTheme}>Toggle theme</button>
-            <List theme={currentTheme} data={data} />
-        </div>
+        <ThemeProvider theme={currentTheme}>
+            <div className={className}>
+                <button onClick={changeTheme}>Toggle theme</button>
+                <List data={data} />
+            </div>
+        </ThemeProvider>
     );
 }
 
-function List(props: { theme: Theme; data: IItem[] }) {
+function List(props: { data: IItem[] }) {
+    const theme = useTheme();
     return (
         <div>
             {data.map((item) => (
-                <ListItem
-                    theme={props.theme}
-                    caption={item.name}
-                    key={item.id}
-                />
+                <ListItem theme={theme} caption={item.name} key={item.id} />
             ))}
         </div>
     );
 }
-
 function ListItem(props: { theme: Theme; caption: string }) {
     const className = `listItem listItem_${props.theme}`;
     return <div className={className}>{props.caption}</div>;
